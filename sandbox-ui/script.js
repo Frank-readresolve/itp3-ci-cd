dc8371237be4967be7c2b4ac6707a282de185536
ThomasLuton <luton.thomas60@gmail.com>
2023-07-24 14:10:46 +0200

    add env.json

--- sandbox-ui/script.js
+++ sandbox-ui/script.js
@@ -1,8 +1,11 @@
 const target = document.getElementById("target");
 
-const url = "http://localhost:8080/"
+const url = "http://localhost:8080/";
 
 async function getData() {
+    const envResponse = await fetch("./env.json");
+    const envJson = await envResponse.json();
+    const url = envJson.URL_BACK;
     const options = {
         method: "get"
     }
